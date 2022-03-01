23ae464f526786a11bb02e99abd0a56e72210e75
cristiansarabiakibernum <cristian.sarabia@kibernum.com>
2022-03-01 13:35:17 -0300

    modifico archivo de servicio

--- src/app/services/noticia.service.ts
+++ src/app/services/noticia.service.ts
@@ -13,17 +13,6 @@ export class NoticiaService {
 
   
   getNoticias(parametros: any): Observable<any> {
-    // const URL = 'https://newsapi.org/v2/top-headlines?country='
-    //   + parametros.pais 
-    //   + '&category=' + parametros.categoria 
-    //   + '&apiKey=588731edf7b04824ac9df4e3016d0d76';
- 
-      //https://newsdata.io/api/1/news?apikey=pub_48711d29e6968c9493a901faefb61fe3f7df&country=ar&language=es,en&category=sports
-
-      // const URL = 'https://newsdata.io/api/1/news?apikey=pub_48711d29e6968c9493a901faefb61fe3f7df'
-      // + '&country=' + 'ar' 
-      // + '&language=es' 
-      // + '&category=sports'
     
       const URL = 'https://newsdata.io/api/1/news?apikey=pub_48711d29e6968c9493a901faefb61fe3f7df'
       + '&country=' + parametros.pais 
